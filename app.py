732ebb944e9878e4f15bcbb87991f273b3f7b3b5
Ezinwanne Aka <ezinwanneaka@gmail.com>
2025-01-11 17:50:57 +0100

    'update'

---
+++ app.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import random
+
+# Load your dataset
+df = pd.read_parquet('all_models.parquet')
+
+st.title('Data Sampling App')   
+
+specialties = df['specialty'].unique()
+selected_specialty = st.selectbox('Select Specialty:', specialties)
+model = st.selectbox('Select Model:', df['model'].unique())
+question = st.selectbox('Select Question:', df['question_type'].unique())
+
+if st.button('Generate Sample'):
+    filtered_df = df[(df['specialty'] == selected_specialty) & (df['model'] == model) & (df['question_type'] == question)]
+    filtered_true = filtered_df[filtered_df['correct'] == 1]
+    filtered_false = filtered_df[filtered_df['correct'] == 0]
+    num = np.random.randint(10000)
+
+    if len(filtered_true) > 2:
+        filtered_true = filtered_true.sample(n=5,random_state=num)
+    if len(filtered_false) > 8:
+        filtered_false = filtered_false.sample(n=5,random_state=num)
+    
+    true_false_df = pd.concat([filtered_true, filtered_false],ignore_index=True)
+    
+    st.write(true_false_df[['sample_id', 'question', 'correct_answer2', 'model', 'model_answer', 'Correct and consistent with scientific consensus',
+       'Requires African local expertise', 'Omission of relevant info',
+       'Includes irrelevant- wrong- or extraneous information',
+       'Evidence of correct reasoning or logic',
+       'Indication of demographic bias', 'Possibility of harm',
+       'Question Quality', 'Formatting- style- structure or grammar issues',
+       'user_id']])
+    
+    @st.cache
+    def convert_df_to_csv(df1):
+        return df1.to_csv(index=False).encode('utf-8')
+
+    csv = convert_df_to_csv(true_false_df)
+    st.download_button(
+        label="Download CSV",
+        data=csv,
+        file_name='sample_data.csv',
+        mime='text/csv'
+    )
