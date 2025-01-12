ea0e94994b1ef2d063d218cfc9a96a5d5ff9613a
Ezinwanne Aka <ezinwanneaka@gmail.com>
2025-01-12 03:19:54 +0100

    updated dataset

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import random
-import warnings
+import warnings 
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 # Load your dataset
